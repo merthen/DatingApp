4d94ac918f7d59dd8c3e099a7370ba448b174dce
morosz <mate.orosz@computacenter.com>
2019-12-09 16:53:15 +0100

    DB redone. AuthRepository created

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -29,6 +29,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddControllers();
         }
 
