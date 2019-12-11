c0273b4507b17b3023278d088eea76a4ac51e5e7
morosz <mate.orosz@computacenter.com>
2019-12-11 16:17:19 +0100

    End of Day commit

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-value',
-  templateUrl: './value.component.html',
-  styleUrls: ['./value.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class ValueComponent implements OnInit {
+export class HomeComponent implements OnInit {
+
+  registerMode = false;
   values: any;
   constructor(private http: HttpClient) { }
 
@@ -14,6 +16,13 @@ export class ValueComponent implements OnInit {
     this.getValues();
   }
 
+  registerToggle(){
+    this.registerMode = true;
+  }
+
+  cancelRegisterMode(registerMode: boolean){
+    this.registerMode = registerMode;
+  }
   getValues(){
     this.http.get('http://localhost:8080/api/values').subscribe(response => {
       this.values = response;
